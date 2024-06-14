632d9da20ac3c0e27239cdf1ec877863afa2fe39
Pritesh Parihar <priteshparihar19@gmail.com>
2024-06-14 16:19:10 +0530

    hey

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div>
+    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+
+    <h1>we learn how to install livewire package</h1>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Livewire_1 </title>
+
+    @livewireStyles
+</head>
+<body>
+    
+    @livewire('counter')
+
+    @livewireScripts
+</body>
+</html>
